b9ef13c5f0711e1c53b5b435779866e77fde4cfa
kCottrell-dev <73491964+kCottrell-dev@users.noreply.github.com>
2021-05-04 21:27:03 -0500

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,56 @@
+
+package server;
+
+import java.awt.GridLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.net.Socket;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.border.TitledBorder;
+
+public class Client extends JFrame {
+ 
+ private JTextArea area1 = new JTextArea();
+ private JTextArea area2 = new JTextArea();
+ 
+ public Client() {
+  setLayout(new GridLayout(2, 1, 5, 5));
+     JScrollPane SP1 = new JScrollPane(area1);
+     JScrollPane SP2 = new JScrollPane(area2);
+     SP1.setBorder(new TitledBorder("Server"));
+     SP2.setBorder(new TitledBorder("Client"));
+     add(SP1);
+     add(SP2);
+  setTitle("Client");
+  setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  setSize(500, 500);
+  setVisible(true);
+  
+  area2.addKeyListener(new KeyAdapter() {
+   @Override
+   public void keyPressed(KeyEvent e) {
+          if(e.getSource() == area2)
+       {
+           System.out.println(area2.getText());
+       }
+       
+   }
+  });
+  
+  try {
+   Socket socket = new Socket("localhost", 8000); 
+  } catch (IOException e) {
+   e.printStackTrace();
+  }
+ }
+ 
+
+
+ public static void main(String[] args) {
+  new Client();
+ }
+}
